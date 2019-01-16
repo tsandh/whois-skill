3b09c8181dc4f99ad844e962c827344268cab5ee
Thomas Sandholm <t.sandholm@cablelabs.com>
2019-01-15 16:44:54 -0800

    init bug fix

--- __init__.py
+++ __init__.py
@@ -58,4 +58,4 @@ class WhoIsSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return SpeedTestSkill()
+    return WhoIsSkill()
